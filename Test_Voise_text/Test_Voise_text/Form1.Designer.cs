386356aec40a773b086ce5966b47074295cfd49b
Pasha <mr.hodovaniukpavel@gmail.com>
2016-11-19 13:27:51 +0200

    1

--- Test_Voise_text/Test_Voise_text/Form1.Designer.cs
+++ Test_Voise_text/Test_Voise_text/Form1.Designer.cs
@@ -82,6 +82,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnGo);
             this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
