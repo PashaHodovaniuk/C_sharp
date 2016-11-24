fde01d45e7b9197043b725690cf138257f07e1e2
Pasha <mr.hodovaniukpavel@gmail.com>
2016-11-24 22:03:46 +0200

    обработка команды

--- Voice command/Voice command/Form1.Designer.cs
+++ Voice command/Voice command/Form1.Designer.cs
@@ -28,12 +28,270 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_edit = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileTool = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runTool = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolS = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolS = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpTool = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DG_number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DG_Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DB_PCommand = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DG_FilePath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DG_CloseProc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_add
+            // 
+            this.btn_add.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add.Location = new System.Drawing.Point(679, 27);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(180, 62);
+            this.btn_add.TabIndex = 0;
+            this.btn_add.Text = "Add process";
+            this.btn_add.UseVisualStyleBackColor = true;
+            // 
+            // btn_edit
+            // 
+            this.btn_edit.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_edit.Location = new System.Drawing.Point(679, 95);
+            this.btn_edit.Name = "btn_edit";
+            this.btn_edit.Size = new System.Drawing.Size(180, 62);
+            this.btn_edit.TabIndex = 1;
+            this.btn_edit.Text = "Edit process";
+            this.btn_edit.UseVisualStyleBackColor = true;
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_delete.Location = new System.Drawing.Point(679, 163);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(180, 62);
+            this.btn_delete.TabIndex = 2;
+            this.btn_delete.Text = "Delete process";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            // 
+            // btn_start
+            // 
+            this.btn_start.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_start.Location = new System.Drawing.Point(679, 231);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(180, 62);
+            this.btn_start.TabIndex = 3;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Enabled = false;
+            this.btn_stop.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_stop.Location = new System.Drawing.Point(679, 299);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(180, 62);
+            this.btn_stop.TabIndex = 4;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileTool,
+            this.runTool,
+            this.helpTool});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menuStrip1.Size = new System.Drawing.Size(871, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileTool
+            // 
+            this.fileTool.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileTool.Name = "fileTool";
+            this.fileTool.Size = new System.Drawing.Size(37, 20);
+            this.fileTool.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // runTool
+            // 
+            this.runTool.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolS,
+            this.stopToolS});
+            this.runTool.Name = "runTool";
+            this.runTool.Size = new System.Drawing.Size(40, 20);
+            this.runTool.Text = "Run";
+            // 
+            // startToolS
+            // 
+            this.startToolS.Name = "startToolS";
+            this.startToolS.Size = new System.Drawing.Size(98, 22);
+            this.startToolS.Text = "Start";
+            this.startToolS.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // stopToolS
+            // 
+            this.stopToolS.Enabled = false;
+            this.stopToolS.Name = "stopToolS";
+            this.stopToolS.Size = new System.Drawing.Size(98, 22);
+            this.stopToolS.Text = "Stop";
+            this.stopToolS.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // helpTool
+            // 
+            this.helpTool.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem1,
+            this.aboutToolStripMenuItem});
+            this.helpTool.Name = "helpTool";
+            this.helpTool.Size = new System.Drawing.Size(44, 20);
+            this.helpTool.Text = "Help";
+            // 
+            // helpToolStripMenuItem1
+            // 
+            this.helpToolStripMenuItem1.Name = "helpToolStripMenuItem1";
+            this.helpToolStripMenuItem1.Size = new System.Drawing.Size(107, 22);
+            this.helpToolStripMenuItem1.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_exit.Location = new System.Drawing.Point(679, 367);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(180, 62);
+            this.btn_exit.TabIndex = 7;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.DG_number,
+            this.DG_Name,
+            this.DB_PCommand,
+            this.DG_FilePath,
+            this.DG_CloseProc});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(661, 414);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // DG_number
+            // 
+            this.DG_number.HeaderText = "№";
+            this.DG_number.Name = "DG_number";
+            this.DG_number.Width = 30;
+            // 
+            // DG_Name
+            // 
+            this.DG_Name.HeaderText = "Name command";
+            this.DG_Name.Name = "DG_Name";
+            this.DG_Name.Width = 150;
+            // 
+            // DB_PCommand
+            // 
+            this.DB_PCommand.HeaderText = "Pronunciation command";
+            this.DB_PCommand.Name = "DB_PCommand";
+            this.DB_PCommand.Width = 150;
+            // 
+            // DG_FilePath
+            // 
+            this.DG_FilePath.HeaderText = "File path";
+            this.DG_FilePath.Name = "DG_FilePath";
+            this.DG_FilePath.Width = 200;
+            // 
+            // DG_CloseProc
+            // 
+            this.DG_CloseProc.HeaderText = "Closed processes";
+            this.DG_CloseProc.Name = "DG_CloseProc";
+            this.DG_CloseProc.Width = 70;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(871, 453);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_edit);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Voice command";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_edit;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileTool;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runTool;
+        private System.Windows.Forms.ToolStripMenuItem startToolS;
+        private System.Windows.Forms.ToolStripMenuItem stopToolS;
+        private System.Windows.Forms.ToolStripMenuItem helpTool;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DG_number;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DG_Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DB_PCommand;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DG_FilePath;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DG_CloseProc;
     }
 }
 
--- Voice command/Voice command/Form1.cs
+++ Voice command/Voice command/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Threading;
+using System.Diagnostics;
+
 
 namespace Voice_command
 {
@@ -15,6 +19,128 @@ namespace Voice_command
         public Form1()
         {
             InitializeComponent();
+        }
+        public Choices cList1 = new Choices();
+        SpeechRecognitionEngine sReconize = new SpeechRecognitionEngine();
+        Choices cList2 = new Choices();
+        Process myProcess;
+        List<string> killProcess = new List<string>();
+        string[] killProc;
+        string[] call;
+        string[] path;
+        int numberOfRecords;
+        int indexOfProcess = 0;
+        bool flag = false;
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        public void addCall(int count)
+        {
+            call = new string[count+2];
+            killProc = new string[count];
+            path = new string[count];
+            call[0] = "Jaliks";
+            call[1] = "exit";
+            for (int i = 0; i < count; i++)
+            {
+                call[i+2] = dataGridView1[2, i].Value.ToString();
+                path[i] = dataGridView1[3, i].Value.ToString();
+            }
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            btn_add.Enabled = false;
+            btn_edit.Enabled = false;
+            btn_start.Enabled = false;
+            btn_delete.Enabled = false;
+            startToolS.Enabled = false;
+            stopToolS.Enabled = true;
+            btn_stop.Enabled = true;
+            numberOfRecords = dataGridView1.RowCount-1;
+            addCall(numberOfRecords);
+            cList2.Add(call);
+            Grammar gr = new Grammar(new GrammarBuilder(cList2));
+            try
+            {
+                sReconize.RequestRecognizerUpdate();
+                sReconize.LoadGrammar(gr);
+                sReconize.SpeechRecognized += sReconize_SpeechRecognized;
+                sReconize.SetInputToDefaultAudioDevice();
+                sReconize.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch
+            {
+                MessageBox.Show("Err1", "Message!");
+                return;
+            }
+        }
+
+        private void sReconize_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (flag)
+            {
+                runCommand(e);
+                flag = false;
+            }
+            if (e.Result.Text.ToString() == "exit")
+            {
+                Application.Exit();
+            }
+            else
+            {
+                if (e.Result.Text.ToString() == "Jaliks")
+                {
+                    flag = true;
+                }
+            }            
+    }
+
+        private void runCommand(SpeechRecognizedEventArgs e)
+        {
+            foreach (string temp in call)
+            {
+                if (e.Result.Text.ToString() == temp)
+                {
+                    string s = temp;
+                    char c = 'C';
+                    int index = s.IndexOf(c);
+                    if (index == 0)
+                    {
+                        if (indexOfProcess == 1)
+                        {
+                            CloseProcess();
+                        }                        
+                    }
+                    else
+                    {
+                        StartProcess();
+                    }
+                }
+            }
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            sReconize.RecognizeAsyncStop();
+            btn_add.Enabled = true;
+            btn_edit.Enabled = true;
+            btn_start.Enabled = true;
+            btn_delete.Enabled = true;
+            startToolS.Enabled = true;
+            stopToolS.Enabled = false;
+            btn_stop.Enabled = false;
         }
     }
 }
